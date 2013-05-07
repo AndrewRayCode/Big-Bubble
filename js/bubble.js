b7f5ca1ba1e33c766943b65239e89a07742ee9c5
Andrew Ray <delvarworld@gmail.com>
2013-05-06 23:16:22 -0700

    Removing code and settint stage size?

--- js/bubble.js
+++ js/bubble.js
@@ -1,19 +1,21 @@
 (function() {
 
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
+var stage = {
+    width: 400, // window.innerWidth
+    height: 600 // window.innerHeight
+};
 
-if ( ! Detector.webgl ) {
-    Detector.addGetWebGLMessage();
+if ( ! window.Detector.webgl ) {
+    window.Detector.addGetWebGLMessage();
 }
     
-var $container = $('<div></div>').appendTo(document.body);
-var renderer = new THREE.WebGLRenderer( { antialias: false } );
-renderer.setSize( window.innerWidth, window.innerHeight );
+var $container = $('<div></div>').css({ height: '200px', width: '400px' }).appendTo(document.body);
+var renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setSize( stage.width, stage.height );
 renderer.autoClear = false;
 $container.append( renderer.domElement );
 
-var camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 100000 );
+var camera = new THREE.PerspectiveCamera( 60, stage.width / stage.height, 1, 100000 );
 camera.position.z = 3200;
 
 var scene = new THREE.Scene();
@@ -69,10 +71,9 @@ for ( var i = 0; i < 500; i ++ ) {
 }
 
 var animate = function() {
-    requestAnimationFrame( animate );
+    window.requestAnimationFrame( animate );
     render();
 };
-
 
 var inertia = { x: 0, y: 0 },
     moving,
@@ -178,130 +179,4 @@ var keyListen = function(key) {
     keyListen(key);
 });
 
-
 }());
-
-function test() {
-var container;
-
-var camera, scene, renderer;
-var cameraCube, sceneCube;
-
-var mesh, zmesh, lightMesh, geometry;
-var spheres = [];
-
-var directionalLight, pointLight;
-
-var mouseX = 0, mouseY = 0;
-
-//document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-
-function init() {
-// create a point light
-
-    for ( var i = 0; i < 500; i ++ ) {
-
-        var mesh = new THREE.Mesh( geometry, material );
-
-        mesh.position.x = Math.random() * 10000 - 5000;
-        mesh.position.y = Math.random() * 10000 - 5000;
-        mesh.position.z = 0;
-
-        mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 3 + 1;
-
-        scene.add( mesh );
-
-        spheres.push( mesh );
-
-    }
-
-    scene.matrixAutoUpdate = false;
-
-    // Skybox
-
-    var shader = THREE.ShaderLib[ "cube" ];
-    //shader.uniforms[ "tCube" ].value = textureCube;
-
-    var material = new THREE.ShaderMaterial( {
-
-        fragmentShader: shader.fragmentShader,
-        vertexShader: shader.vertexShader,
-        uniforms: shader.uniforms,
-        side: THREE.BackSide
-
-    } ),
-
-    mesh = new THREE.Mesh( new THREE.CubeGeometry( 100000, 100000, 100000 ), material );
-    sceneCube.add( mesh );
-
-    //
-
-    renderer = new THREE.WebGLRenderer( { antialias: false } );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.autoClear = false;
-    container.appendChild( renderer.domElement );
-
-    //
-
-    window.addEventListener( 'resize', onWindowResize, false );
-
-}
-
-function onWindowResize() {
-
-    windowHalfX = window.innerWidth / 2;
-    windowHalfY = window.innerHeight / 2;
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    cameraCube.aspect = window.innerWidth / window.innerHeight;
-    cameraCube.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
-
-function onDocumentMouseMove( event ) {
-
-    mouseX = ( event.clientX - windowHalfX ) * 10;
-    mouseY = ( event.clientY - windowHalfY ) * 10;
-
-}
-
-//
-
-function animate() {
-    requestAnimationFrame( animate );
-    render();
-}
-
-function render() {
-
-    var timer = 0.0001 * Date.now();
-
-    camera.position.x = 0;
-    camera.position.y = 0;
-
-    camera.lookAt( scene.position );
-
-    cameraCube.rotation.copy( camera.rotation );
-
-    for ( var i = 0, il = spheres.length; i < il; i ++ ) {
-
-        var sphere = spheres[ i ];
-
-        //sphere.position.x = 5000 * Math.cos( timer + i );
-        //sphere.position.y = 5000 * Math.sin( timer + i * 1.1 );
-
-    }
-
-    renderer.clear();
-    //renderer.render( sceneCube, cameraCube );
-    renderer.render( scene, camera );
-
-}
-
-init();
-animate();
-}
